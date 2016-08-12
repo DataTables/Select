ff2d399e257c1b1a246831aaa3601e0276a7934b
Allan Jardine <allan.jardine@sprymedia.co.uk>
2016-08-12 14:21:22 +0100

    Dev: Fix disabling of mouse selection after previous commit changing the selector container node

--- js/dataTables.select.js
+++ js/dataTables.select.js
@@ -305,7 +305,7 @@ function disableMouseSelection( dt )
 	var ctx = dt.settings()[0];
 	var selector = ctx._select.selector;
 
-	$( dt.table().body() )
+	$( dt.table().container() )
 		.off( 'mousedown.dtSelect', selector )
 		.off( 'mouseup.dtSelect', selector )
 		.off( 'click.dtSelect', selector );
