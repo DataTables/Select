91ef6ee3c9a7be8b6ee61c774fa418acd01f61c8
Allan Jardine <allan@datatables.net>
2021-12-15 09:33:37 +0000

    Release 1.3.4

--- js/dataTables.select.js
+++ js/dataTables.select.js
@@ -1,4 +1,4 @@
-/*! Select for DataTables 1.3.4-dev
+/*! Select for DataTables 1.3.4
  * 2015-2021 SpryMedia Ltd - datatables.net/license/mit
  */
 
@@ -6,7 +6,7 @@
  * @summary     Select for DataTables
  * @description A collection of API methods, events and buttons for DataTables
  *   that provides selection options of the items in a DataTable
- * @version     1.3.4-dev
+ * @version     1.3.4
  * @file        dataTables.select.js
  * @author      SpryMedia Ltd (www.sprymedia.co.uk)
  * @contact     datatables.net/forums
@@ -54,7 +54,7 @@ var DataTable = $.fn.dataTable;
 // Version information for debugger
 DataTable.select = {};
 
-DataTable.select.version = '1.3.4-dev';
+DataTable.select.version = '1.3.4';
 
 DataTable.select.init = function ( dt ) {
 	var ctx = dt.settings()[0];
