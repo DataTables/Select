580888daabe5280376208f57ec821ef14579274b
Allan Jardine <allan@datatables.net>
2023-02-28 10:14:05 +0000

    Release 1.6.1

--- js/dataTables.select.js
+++ js/dataTables.select.js
@@ -1,11 +1,11 @@
-/*! Select for DataTables 1.6.0
+/*! Select for DataTables 1.6.1
  * 2015-2023 SpryMedia Ltd - datatables.net/license/mit
  */
 
 // Version information for debugger
 DataTable.select = {};
 
-DataTable.select.version = '1.6.0';
+DataTable.select.version = '1.6.1';
 
 DataTable.select.init = function ( dt ) {
 	var ctx = dt.settings()[0];
