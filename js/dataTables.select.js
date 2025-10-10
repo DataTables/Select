f63594b13a90c973130d0cad0c848479681cf4f8
Allan Jardine <allan@datatables.net>
2025-10-10 08:55:29 +0000

    Release 3.1.3

--- js/dataTables.select.js
+++ js/dataTables.select.js
@@ -1,4 +1,4 @@
-/*! Select for DataTables 3.1.2
+/*! Select for DataTables 3.1.3
  * © SpryMedia Ltd - datatables.net/license/mit
  */
 
@@ -9,7 +9,7 @@ DataTable.select.classes = {
 	checkbox: 'dt-select-checkbox'
 };
 
-DataTable.select.version = '3.1.2';
+DataTable.select.version = '3.1.3';
 
 DataTable.select.init = function (dt) {
 	var ctx = dt.settings()[0];
